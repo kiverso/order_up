48d5211570ffb15ab76b01a0c37d6605a4352185
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-29 09:55:32 -0600

    Add calorie count to a dish show page

--- spec/features/dishes/dish_show_spec.rb
+++ spec/features/dishes/dish_show_spec.rb
@@ -27,4 +27,32 @@ RSpec.describe 'Dish show page', type: :feature do
     expect(page).to have_content("#{ingredient3.name}")
     expect(page).to have_content("#{chef1.name}")
   end
+
+  it "can show calorie count" do
+    chef1 = Chef.create(name: "Chef1")
+    dish1 = Dish.create(name: "Dish1",
+                        description: "Mm Food",
+                        chef_id: chef1.id,)
+
+    ingredient1 = Ingredient.create(name: "Cheetos",
+                                    calories: 200)
+    ingredient2 = Ingredient.create(name: "Fritos",
+                                    calories: 200)
+    ingredient3 = Ingredient.create(name: "Doritos",
+                                    calories: 200)
+
+    DishIngredient.create(dish_id: dish1.id,
+                          ingredient_id: ingredient1.id)
+    DishIngredient.create(dish_id: dish1.id,
+                          ingredient_id: ingredient2.id)
+    DishIngredient.create(dish_id: dish1.id,
+                          ingredient_id: ingredient3.id)
+
+    visit("/dishes/#{dish1.id}")
+    expect(page).to have_content("#{ingredient1.name}")
+    expect(page).to have_content("#{ingredient2.name}")
+    expect(page).to have_content("#{ingredient3.name}")
+    expect(page).to have_content("#{chef1.name}")
+    expect(page).to have_content("Calories: #{dish1.total_calories}")
+  end
 end
