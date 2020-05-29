2098ef6d9b0c252e014a5d04b0eae8f33f1fdee6
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-29 09:52:26 -0600

    Add total_calories method and test to dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe Dish, type: :model do
     it {should belong_to :chef}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe 'instance methods' do
+    it 'can get total_calories' do
+      chef1 = Chef.create(name: "Chef1")
+      dish1 = Dish.create(name: "Dish1",
+                          description: "Mm Food",
+                          chef_id: chef1.id,)
+
+      ingredient1 = Ingredient.create(name: "Cheetos",
+                                      calories: 200)
+      ingredient2 = Ingredient.create(name: "Fritos",
+                                      calories: 200)
+      ingredient3 = Ingredient.create(name: "Doritos",
+                                      calories: 200)
+
+      DishIngredient.create(dish_id: dish1.id,
+                            ingredient_id: ingredient1.id)
+      DishIngredient.create(dish_id: dish1.id,
+                            ingredient_id: ingredient2.id)
+      DishIngredient.create(dish_id: dish1.id,
+                            ingredient_id: ingredient3.id)
+
+      expect(dish1.total_calories).to eq(600)
+    end
+  end
 end
